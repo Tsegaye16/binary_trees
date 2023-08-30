6a2808242c00de48e46e789b874e0dcb5acee62d
Tsegaye16 <abewatsegaye16@gmail.com>
2023-08-30 00:15:58 -0700

    jdis

--- 2-binary_tree_insert_right.c
+++ 2-binary_tree_insert_right.c
@@ -1,16 +1,16 @@
 #include "binary_trees.h"
 
 /**
- * binary_tree_insert_left - inserts a node as the left-child of another node
- * @parent: pointer to the node to insert the left-child in
- * @value: value to store in the new node
+ * binary_tree_insert_right - inserts a new_node as the right-child of another node
+ * @parent: pointer to the new_node to insert the right-child in
+ * @value: value to store in the newly created node
  *
  * Return: Pointer to the newly created node
  *         NULL on failure
  *         NULL if parent is NULL
  */
 
-binary_tree_t *binary_tree_insert_left(binary_tree_t *parent, int value)
+binary_tree_t *binary_tree_insert_right(binary_tree_t *parent, int value)
 {
 	binary_tree_t *new_node;
 
