91249cc1123a86535f778af8a7b3573e61bef7fc
Tsegaye16 <abewatsegaye16@gmail.com>
2023-08-30 00:29:10 -0700

    dhsfisa

---
+++ 3-binary_tree_delete.c
@@ -0,0 +1,15 @@
+#include "binary_trees.h"
+
+/**
+ * binary_tree_delete : the method that deletes the
+ *	entire binary tree
+ * @tree :  a binary tree to be deleted
+ */
+void binary_tree_delete(binary_tree_t *tree)
+{
+	if (!tree)
+		return;
+	binary_tree_delete(tree->left);
+	binary_tree_delete(tree->right);
+	free(tree);
+}
