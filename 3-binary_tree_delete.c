5db6f88331ad26cce47c183988ddbf7d81c9b976
Tsegaye16 <abewatsegaye16@gmail.com>
2023-08-30 00:31:56 -0700

    fjdk

--- 3-binary_tree_delete.c
+++ 3-binary_tree_delete.c
@@ -1,9 +1,9 @@
 #include "binary_trees.h"
 
 /**
- * binary_tree_delete : the method that deletes the
+ * binary_tree_delete - the method that deletes the
  *	entire binary tree
- * @tree :  a binary tree to be deleted
+ * @tree: a binary tree to be deleted
  */
 void binary_tree_delete(binary_tree_t *tree)
 {
