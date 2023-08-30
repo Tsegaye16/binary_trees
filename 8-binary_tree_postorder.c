4592188db0503d68d91ce879d2f0baa160cf779d
Tsegaye16 <abewatsegaye16@gmail.com>
2023-08-30 02:02:42 -0700

    binary tree traversal

--- 8-binary_tree_postorder.c
+++ 8-binary_tree_postorder.c
@@ -11,6 +11,6 @@ void binary_tree_postorder(const binary_tree_t *tree, void (*func)(int))
 	if (!tree || !func)
 		return;
 	binary_tree_postorder(tree->left, func);
-	binary_tree_postorder(tree->left, func);
+	binary_tree_postorder(tree->right, func);
 	func(tree->n);
 }
