ce4e6658d2ed27db8894150ad891c9beae1dfe19
Tsegaye16 <abewatsegaye16@gmail.com>
2023-08-30 00:18:04 -0700

    hfjs

--- 2-binary_tree_insert_right.c
+++ 2-binary_tree_insert_right.c
@@ -1,7 +1,8 @@
 #include "binary_trees.h"
 
 /**
- * binary_tree_insert_right - inserts a new_node as the right-child of another node
+ * binary_tree_insert_right - inserts a new_node as
+ *	the right-child of another node
  * @parent: pointer to the new_node to insert the right-child in
  * @value: value to store in the newly created node
  *
