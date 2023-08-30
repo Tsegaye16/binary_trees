6bbd595a519dd5e7d6b1ab13c5bd628acc979a9c
Tsegaye16 <abewatsegaye16@gmail.com>
2023-08-30 01:50:28 -0700

    hdsjk

--- 9-binary_tree_height.c
+++ 9-binary_tree_height.c
@@ -12,8 +12,11 @@ size_t binary_tree_height(const binary_tree_t *tree)
 {
 	if (!tree)
 		return (0);
-	size_t left_height = tree->left ? binary_tree_height(tree->left) :  0;
-	size_t right_height = tree->right ? binary_tree_height(tree->right) : 0;
+	size_t left_height = 0;
+	size_t right_height = 0;
+
+	left_height = tree->left ? binary_tree_height(tree->left) :  0;
+	right_height = tree->right ? binary_tree_height(tree->right) : 0;
 
 	return (1 + (right_height > left_height ? right_height : left_height));
 }
