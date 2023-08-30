98267c2c28546bdf367fcb2744897588f119ab5b
Tsegaye16 <abewatsegaye16@gmail.com>
2023-08-30 04:15:34 -0700

    jshjds

--- 14-binary_tree_balance.c
+++ 14-binary_tree_balance.c
@@ -17,7 +17,7 @@ int binary_tree_balance(const binary_tree_t *tree)
 		return (0);
 	left_height = tree->left ? (int)binary_tree_height(tree->left) : -1;
 	right_height = tree->right ? (int)binary_tree_height(tree->right) : -1;
-	return (right_height - left_height);
+	return (left_height - right_height);
 }
 
 /**
