8248fc3bcdbaf659d66046525707c7321b8ca442
Tsegaye16 <abewatsegaye16@gmail.com>
2023-08-31 01:27:12 -0700

    find sibling

--- 17-binary_tree_sibling.c
+++ 17-binary_tree_sibling.c
@@ -10,7 +10,7 @@
  */
 binary_tree_t *binary_tree_sibling(binary_tree_t *node)
 {
-	if (node == NULL || node->parent ==NULL)
+	if (node == NULL || node->parent == NULL)
 		return (NULL);
 	if (node == node->parent->right)
 		return (node->parent->left);
