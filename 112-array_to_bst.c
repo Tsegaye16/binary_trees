29da1790244a3fc6e62f7ec7bc2f679a61715dea
Tsegaye16 <abewatsegaye16@gmail.com>
2023-08-31 13:32:57 -0700

    array to bst

---
+++ 112-array_to_bst.c
@@ -0,0 +1,18 @@
+#include "binary_trees.h"
+/**
+ * array_to_bst - a function used to for BST from array
+ * @array: is a pointer to the first element of the array to be converted
+ * @size: is the number of element in the array
+ *
+ * Return: newly formed BST
+ */
+bst_t *array_to_bst(int *array, size_t size)
+{
+	bst_t *root;
+	size_t k;
+
+	for (k = 0; k < size; k++)
+		bst_insert(&root, array[k]);
+	return (root);
+
+}
